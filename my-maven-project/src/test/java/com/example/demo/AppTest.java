159bc0a0f928bcbc8dced01ba1885b23348fbf46
Kantimahanty Rohit <rohit.apex1999@gmail.com>
2023-02-15 12:27:45 +0530

    my demo maven project

---
+++ my-maven-project/src/test/java/com/example/demo/AppTest.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    //@Test
+    //public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void test(){System.out.println("This is the test number 1");}
+
+    @Before
+    public void bef(){System.out.println("This is before the test number 1");}
+
+    @After
+    public void aft(){System.out.println("This is afetr the test number 1");}
+
+    @Test
+    public void test2(){
+        System.out.println("This is test 2 using assertions");
+        assertEquals(9,8);
+    }
+}
